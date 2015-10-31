ffd419c462f645938557e49bfab5b1c8192c4032
danleyb2 <ndieksman@gmail.com>
2015-10-31 11:28:17 +0300

    configs for blog app

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from blog import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^blog/',include('blog.urls'),name='blog'),
+    url(r'^$',views.index),
 
     url(r'^admin/', include(admin.site.urls)),
 )
