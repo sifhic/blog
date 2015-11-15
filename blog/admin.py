32ca09e80b52c4106f3e91bc63b734652fbf8783
danleyb2 <ndieksman@gmail.com>
2015-11-15 17:30:06 +0300

    add tinymce for easy  html markup editing

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from blog.models import Post,Comment
-
+from django.db import models
+from tinymce.widgets import TinyMCE
 # Register your models here.
 
 class CommentAdmin(admin.StackedInline):
@@ -9,6 +10,15 @@ class CommentAdmin(admin.StackedInline):
 
 
 class PostAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField:{'widget':TinyMCE(
+            attrs={'cols':150,'rows':30},
+            mce_attrs={
+                'nowrap': True,
+                'toolbar': "undo redo | styleselect | bold italic ",
+                },
+        )}
+    }
     fieldsets = [
         ('creator',{'fields':['creator']}),
         (None,  {'fields' : ['heading','body']}),
