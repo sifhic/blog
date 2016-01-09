38e15fbf4440bcf5e20e257984835e2cbf4b5d8e
danleyb2 <ndieksman@gmail.com>
2016-01-09 16:55:22 +0300

    add STATICFILES_DIRS to store css,js libs

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,9 +105,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
-STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_libs').replace('\\', '/'),
 
 
-)
+]
 STATIC_URL = '/static/'
