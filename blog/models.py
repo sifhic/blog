2e55a36389ec5e1ec8dccb02848c2745b940c1aa
Nyaundi Brian <ndieksman@gmail.com>
2016-03-21 04:36:37 +0000

    add post tags and features image

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth.admin import User
 
-# Create your models here.
+    
 
 class Post(models.Model):
     def __str__(self):
         return self.heading
     creator=models.ForeignKey(User)
     heading=models.CharField(max_length=200)
+    sub_heading=models.CharField(max_length=300)
     body=models.TextField()
+    is_published = models.BooleanField(default=0)
+    featured_image = models.ImageField(upload_to='featured_images')
     pub_date=models.DateTimeField('date published', auto_now_add=True)
+    
+# Create your models here.
+class Tag(models.Model):
+    name=models.CharField(max_length=60)
+    post = models.ManyToManyField(Post,related_name="tags")
 
 class Comment(models.Model):
     def __str__(self):
