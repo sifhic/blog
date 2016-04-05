885a0dc9c70bdb38da1b6866676c7915db2170d5
Nyaundi Brian <ndieksman@gmail.com>
2016-04-05 01:25:40 +0000

    switch to django-suit CKEditor from tinymce

--- blog/admin.py
+++ blog/admin.py
@@ -1,27 +1,51 @@
 from django.contrib import admin
 from blog.models import Post,Comment
 from django.db import models
-from tinymce.widgets import TinyMCE
+from django.forms import ModelForm
+from django.contrib.admin import ModelAdmin
+from suit_ckeditor.widgets import CKEditorWidget
+
 # Register your models here.
 
 class CommentAdmin(admin.StackedInline):
     model = Comment
     extra = 1
 
+class PostForm(ModelForm):
+    class Meta:
+        #model = Post
+        _ck_editor_toolbar = [
+                {'name': 'basicstyles', 'groups': ['basicstyles', 'cleanup']},
+                {'name': 'paragraph',
+                 'groups': ['list', 'indent', 'blocks', 'align']},
+                {'name': 'document', 'groups': ['mode']}, '/',
+                {'name': 'styles'}, {'name': 'colors'},
+                {'name': 'insert_custom',
+                 'items': ['Image', 'Flash', 'Table', 'HorizontalRule']},
+                {'name': 'about'}]
+    
+        _ck_editor_config = {'autoGrow_onStartup': True,
+                             'autoGrow_minHeight': 100,
+                             'autoGrow_maxHeight': 250,
+                             'extraPlugins': 'autogrow',
+                             'toolbarGroups': _ck_editor_toolbar}
+        
+        widgets = {
+            'body': CKEditorWidget(editor_options=_ck_editor_config)
+        }
 
 class PostAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.TextField:{'widget':TinyMCE(
-            attrs={'cols':150,'rows':30},
-            mce_attrs={
-                'nowrap': True,
-                'toolbar': "undo redo | styleselect | bold italic ",
-                },
-        )}
-    }
+    form=PostForm
     fieldsets = [
-        ('creator',{'fields':['creator']}),
-        (None,  {'fields' : ['heading','body']}),
+        ('Post Meta',{
+            'fields':['creator']
+            }),
+        (None,  {'fields' : ['heading','sub_heading','is_published']}),
+        ('Post Body',{
+            'classes': ('grp-collapse grp-closed',),
+            'fields':['body']
+            }),
+        ('Featured Image',{'fields':['featured_image']}),
 
     ]
     inlines = [CommentAdmin]
