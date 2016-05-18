42433aff5115e0586e758234fb14019a2b21b205
Nyaundi Brian <ndieksman@gmail.com>
2016-05-18 17:30:16 +0000

    enable and configure pa app

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'blog',
     'suit_ckeditor',
-    #'pa',
+    'pa',
     #'rog'
 )
 # tinymce configurations
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     url(r'^blog/',include('blog.urls',namespace='blog')),
     url(r'^$',views.index),
-    #url(r'^pa/',include('pa.urls')),
+    url(r'^pa/',include('pa.urls')),
     #url(r'^rog/',include('rog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
