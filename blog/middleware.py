7f788041c36b483454ac702bab10793655d28c15
nyaundi brian <danleyb2@gmail.com>
2019-06-24 23:09:57 +0300

    add site metadata loader middleware

---
+++ blog/middleware.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from blog.models import Post,Category,SiteProfile, Tag
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
+import logging
+
+lgr = logging.getLogger(__name__)
+
+
+class SiteMetaMiddleware(MiddlewareMixin):
+    """Middleware class that retrieves the site metadata
+    """
+
+    def process_request(self, request):
+        """
+        """
+        # todo filter by site
+        site_profile = SiteProfile.objects.last()
+        categories = Category.objects.all()
+        popular_posts = Post.objects.all()[:5]
+        tags = Tag.objects.all()
+
+        request.tags = tags
+        request.site_profile = site_profile
+        request.categories = categories
+        request.popular_posts = popular_posts
