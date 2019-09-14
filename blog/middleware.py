886e4adce21944bd1df3674e3edf73e874e75a35
nyaundi brian <danleyb2@gmail.com>
2019-09-14 10:02:57 +0300

    display categories conditionally

--- blog/middleware.py
+++ blog/middleware.py
@@ -17,12 +17,15 @@ class SiteMetaMiddleware(MiddlewareMixin):
         """
         """
         # todo filter by site
+
+        # todo is this required?, there is already request.site
         site_profile = SiteProfile.objects.last()
-        categories = Category.objects.all()
+        categories = Category.objects.exclude(name='default')
         popular_posts = Post.objects.all()[:5]
         tags = Tag.objects.all()
 
         request.tags = tags
         request.site_profile = site_profile
+        request.display_categories = categories.exists()
         request.categories = categories
         request.popular_posts = popular_posts
